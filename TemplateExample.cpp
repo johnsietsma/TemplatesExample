04d884a4a09f44786c8504a31739e5610a792f86
John Sietsma <johns@ad.aie.edu>
2016-04-04 16:29:06 +1000

    Added particle color updating. - Add particle update function. - Specialized update function for colored particles.

--- TemplateExample.cpp
+++ TemplateExample.cpp
@@ -45,6 +45,33 @@ vec3 calculate_position(const T& physicsObject, float timeStep)
     return physicsObject.velocity * timeStep;
 }
 
+template<typename T>
+vec3 calculate_color(T& object, float deltaTime)
+{
+    static const vec3 colorIncrement{ 0.1f, 0.1f, 0.1f };
+    vec3 c = object.color + colorIncrement * deltaTime;
+    if (c.x > 255) c = { 0,0,0 }; // Wrap the color around
+    return c;
+}
+
+
+template<typename T>
+void update_particles(T* particles, size_t particleCount, float deltaTime)
+{
+    for (size_t i = 0; i < particleCount; i++) {
+        particles[i].position = calculate_position(particles[i], deltaTime);
+    }
+}
+
+template<>
+void update_particles<ParticleColored>(ParticleColored* particles, size_t particleCount, float deltaTime)
+{
+    for (size_t i = 0; i < particleCount; i++) {
+        particles[i].position = calculate_position(particles[i], deltaTime);
+        particles[i].color = calculate_color(particles[i], deltaTime);
+    }
+}
+
 
 // Make and fill a buffer using the defaultObject.
 template<typename T, int TCount, typename... TArgs>
@@ -61,22 +88,19 @@ int main()
     constexpr const size_t ParticleCount = 100; // How many particles we'll have
     constexpr const float TimeStep = 1/30.f;    // How much time passes for each update
 
-    constexpr vec3 origin{ 0,0,0 };
-    constexpr vec3 right{ 1,0,0 };
-    constexpr vec3 green{ 1,0,0 };
+    constexpr const vec3 origin{ 0,0,0 };
+    constexpr const vec3 right{ 1,0,0 };
+    constexpr const vec3 green{ 1,0,0 };
 
     // Create and fill a Particle buffer
     Particle* pParticles = make_buffer<Particle,ParticleCount>(origin, right);
+    // Update particles
+    update_particles(pParticles, ParticleCount, TimeStep);
 
     // Create and fill a ParticleColored buffer
     ParticleColored* pParticlesColored = make_buffer<ParticleColored, ParticleCount>(origin, right, green);
-
-    // Update the position of all the particles.
-    for (size_t particleIndex = 0; particleIndex < ParticleCount; particleIndex++)
-    {
-        pParticles[particleIndex].position += calculate_position(pParticles[particleIndex], TimeStep);
-        pParticlesColored[particleIndex].position += calculate_position(pParticlesColored[particleIndex], TimeStep);
-    }
+    // Update colored particles
+    update_particles(pParticlesColored, ParticleCount, TimeStep);
 
     // Cleanup
     delete pParticles;
